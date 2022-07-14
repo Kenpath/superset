da584915a2338ae0f7838eae6958e0255eb12add
swetha-aggidevara <swetha@intelspace.io>
2022-07-14 13:11:36 +0530

    initial setup

---
+++ superset/superset_config.py
@@ -0,0 +1,9 @@
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://superset:superset@localhost/superset'
+
+
+FEATURE_FLAGS = {
+  "THUMBNAILS": True,
+  "DASHBOARD_NATIVE_FILTERS": True,
+  "OMNIBAR": True
+}
