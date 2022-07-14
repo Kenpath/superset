eb6c34d4693dd3887d2c71e067ead411ede5e615
swetha-aggidevara <swetha@intelspace.io>
2022-07-14 15:47:22 +0530

    added build folder

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,8 +2,8 @@
 # SQLALCHEMY_DATABASE_URI = 'postgresql://superset:superset@localhost/superset'
 
 
-FEATURE_FLAGS = {
-  "THUMBNAILS": True,
-  "DASHBOARD_NATIVE_FILTERS": True,
-  "OMNIBAR": True
-}
+# FEATURE_FLAGS = {
+#   "THUMBNAILS": True,
+#   "DASHBOARD_NATIVE_FILTERS": True,
+#   "OMNIBAR": True
+# }
